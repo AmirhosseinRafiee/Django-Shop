6ccf84c2b4534f6babb360ef944945d03f527923
Amirhossein Rafiee <94180638+AmirhosseinRafiee@users.noreply.github.com>
2024-03-25 00:43:03 +0330

    added base project and also statics for website

---
+++ core/website/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'website'
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('about/', views.AboutView.as_view(), name='index'),
+    path('contact/', views.ContactView.as_view(), name='index'),
+]
+
---
+++ core/website/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    template_name = 'website/index.html'
+
+class AboutView(TemplateView):
+    template_name = 'website/about.html'
+
+class ContactView(TemplateView):
+    template_name = 'website/contact.html'
