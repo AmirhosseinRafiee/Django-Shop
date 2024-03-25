c2f14341d39b9ae85d70f577691faa0e1259ef34
Amirhossein Rafiee <94180638+AmirhosseinRafiee@users.noreply.github.com>
2024-03-25 10:38:06 +0330

    added logout
    and edit navigation to check if user is authenticated or not

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(), name='login'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
     # path('register/', views.RegisterView.as_view(), name='register'),
-    # path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
 ]
 
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
 from django.contrib.auth import views as auth_views
 from .forms import CustomAuthentiactionForm
 
-class LoginView(auth_views.LoginView):
+class CustomLoginView(auth_views.LoginView):
     template_name = "accounts/login.html"
     form_class = CustomAuthentiactionForm
     redirect_authenticated_user = True
-    next_page = '/'
+
+class CustomLogoutView(auth_views.LogoutView):
+    http_method_names = ["post", "options"]
