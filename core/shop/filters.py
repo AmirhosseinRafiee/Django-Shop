8a8db450360b4e950c880135af59670d93c8279e
Amirhossein Rafiee <94180638+AmirhosseinRafiee@users.noreply.github.com>
2024-03-30 05:39:48 +0330

    added filters for product list view

---
+++ core/shop/filters.py
@@ -0,0 +1,34 @@
+import django_filters
+from .models import ProductModel
+
+class ProductFilter(django_filters.FilterSet):
+    q = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
+    price = django_filters.RangeFilter(field_name='discounted_price')
+    category = django_filters.CharFilter(field_name='category__title', lookup_expr='iexact')
+
+    class Meta:
+        model = ProductModel
+        fields = []
+
+    def filter_queryset(self, queryset):
+        queryset = super().filter_queryset(queryset)
+
+        ordering = self._get_ordering()
+        if ordering is not None:
+            queryset = queryset.order_by(ordering)
+
+        return queryset
+
+    def _get_ordering(self):
+        ordering_choices = {
+            'date': 'created_date',
+            '-date': '-created_date',
+            'price': 'discounted_price',
+            '-price': '-discounted_price'
+        }
+        ordering = self.data.get('order_by', None)
+        if ordering:
+            for k, v in ordering_choices.items():
+                if ordering == k:
+                    return v
+        return None
--- core/shop/views.py
+++ core/shop/views.py
@@ -1,15 +1,36 @@
-from django.shortcuts import render
+from django.db.models import F, ExpressionWrapper, IntegerField, Value
 from django.views.generic import ListView, DetailView
-from .models import ProductModel, ProductStatus
+from .models import ProductModel, ProductStatus, ProductCategoryModel
+from .filters import ProductFilter
 
 class ShopProductGridView(ListView):
     template_name = 'shop/product-grid.html'
     queryset = ProductModel.objects.filter(status=ProductStatus.publish.value)
     paginate_by = 9
+    paginate_by_choices = ['9', '18', '27']
+
+    def get_queryset(self):
+        qs = super().get_queryset().prefetch_related('category')
+        discounted_price_expression = ExpressionWrapper(
+            F('price') - (F('price') * F('discount_percent') / Value(100)),
+            output_field=IntegerField()
+        )
+        products_with_discounted_price = qs.annotate(
+            discounted_price=discounted_price_expression
+        )
+        return ProductFilter(self.request.GET, products_with_discounted_price).qs
+
+
+    def get_paginate_by(self, queryset):
+        _paginate_by = self.request.GET.get('page_size', None)
+        if _paginate_by in self.paginate_by_choices:
+            return _paginate_by
+        return self.paginate_by
 
     def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        context["total_items"] = self.get_queryset().count()
+        context = super().get_context_data(**kwargs)
+        context['total_items'] = self.queryset.count()
+        context['categories'] = ProductCategoryModel.objects.all()
         return context
 
 class ShopProductDetailView(DetailView):
