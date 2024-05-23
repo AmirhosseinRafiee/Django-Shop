fccf0855b90cc23556221b4bafecb43ec7c2158d
Amirhossein Rafiee <94180638+AmirhosseinRafiee@users.noreply.github.com>
2024-05-23 14:02:46 +0330

    added showing wished list items

--- core/shop/templatetags/shop_tags.py
+++ core/shop/templatetags/shop_tags.py
@@ -1,21 +1,40 @@
 from django import template
-from ..models import ProductModel, ProductStatus
+from django.db.models import Exists, OuterRef
+from ..models import ProductModel, ProductStatus, WishlistProductModel
 
 register = template.Library()
 
 
-@register.inclusion_tag('includes/latest-products.html')
-def latest_products():
+@register.inclusion_tag('includes/latest-products.html', takes_context=True)
+def latest_products(context):
+    request = context.get('request')
     products = ProductModel.objects.filter(
         status=ProductStatus.publish.value).prefetch_related('category').order_by('-created_date')[:8]
-    return {'products': products}
+    if request.user.is_authenticated:
+        wishlist_subquery = WishlistProductModel.objects.filter(
+            user=request.user,
+            product=OuterRef('pk')
+        )
+        products = products.annotate(
+            in_wishlist=Exists(wishlist_subquery)
+        )
+    return {'products': products, 'request': request}
 
 
-@register.inclusion_tag('includes/similar-products.html')
-def similar_products(product: ProductModel):
+@register.inclusion_tag('includes/similar-products.html', takes_context=True)
+def similar_products(context):
+    request = context.get('request')
+    product = context.get('object')
     products = ProductModel.objects.filter(
         status=ProductStatus.publish.value,
         category__in=product.category.all()
     ).exclude(id=product.id).prefetch_related('category').order_by('-created_date').distinct()[:4]
-    return {'similar_products': products}
-
+    if request.user.is_authenticated:
+        wishlist_subquery = WishlistProductModel.objects.filter(
+            user=request.user,
+            product=OuterRef('pk')
+        )
+        products = products.annotate(
+            in_wishlist=Exists(wishlist_subquery)
+        )
+    return {'similar_products': products, 'request': request}
--- core/shop/views.py
+++ core/shop/views.py
@@ -1,6 +1,9 @@
+from typing import Any
 from django.db.models import F, ExpressionWrapper, IntegerField, Value
+from django.db.models.query import QuerySet
 from django.views.generic import ListView, DetailView
-from .models import ProductModel, ProductStatus, ProductCategoryModel
+from django.db.models import Exists, OuterRef
+from .models import ProductModel, ProductStatus, ProductCategoryModel, WishlistProductModel
 from .filters import ProductFilter
 from .permissons import IsAdminOrPublishedPermission
 
@@ -16,10 +19,23 @@ class ShopProductGridView(ListView):
             F('price') - (F('price') * F('discount_percent') / Value(100)),
             output_field=IntegerField()
         )
-        products_with_discounted_price = qs.annotate(
+        qs = qs.annotate(
             discounted_price=discounted_price_expression
         )
-        return ProductFilter(self.request.GET, products_with_discounted_price).qs
+
+        if self.request.user.is_authenticated:
+            # Subquery to check if the product is in the user's wishlist
+            wishlist_subquery = WishlistProductModel.objects.filter(
+                user=self.request.user,
+                product=OuterRef('pk')
+            )
+
+            # Annotate the products with the existence of the product in the wishlist
+            qs = qs.annotate(
+                in_wishlist=Exists(wishlist_subquery)
+            )
+
+        return ProductFilter(self.request.GET, qs).qs
 
 
     def get_paginate_by(self, queryset):
@@ -36,3 +52,15 @@ class ShopProductGridView(ListView):
 class ShopProductDetailView(IsAdminOrPublishedPermission, DetailView):
     template_name = 'shop/product-detail.html'
     queryset = ProductModel.objects.all().prefetch_related('productimagemodel_set')
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if self.request.user.is_authenticated:
+            wishlist_subquery = WishlistProductModel.objects.filter(
+                user=self.request.user,
+                product=OuterRef('pk')
+            )
+            qs = qs.annotate(
+                in_wishlist=Exists(wishlist_subquery)
+            )
+        return qs
